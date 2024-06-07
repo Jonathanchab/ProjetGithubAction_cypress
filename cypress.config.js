091653dea5b8ff692328e1606c9c68f4cfc21af9
Jonathanchab <jonathanchab@gmail.com>
2024-06-07 11:08:42 +0200

    modification code

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
     setupNodeEvents(on, config) {
-
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
     experimentalOriginDependencies: true,
     video: true
